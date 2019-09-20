ab07c7d11af54a37c2932efe384708b0a6842cf3
github username <github email address>
2019-09-20 09:10:21 +0100

    Done.

--- app/controllers/owners_controller.rb
+++ app/controllers/owners_controller.rb
@@ -1,21 +1,27 @@
 class OwnersController < ApplicationController
 
-  get '/owners' do
-    @owners = Owner.all
-    erb :'/owners/index' 
-  end
+  #CREATE 
 
-  get '/owners/new' do 
+  get '/owners/new' do
+    @pets = Pet.all  
     erb :'/owners/new'
   end
 
   post '/owners' do 
-    
+    @owner = Owner.create(params[:owner])
+
+    if !params["pet"]["name"].empty?
+      @owner.pets << Pet.create(name: params["pet"]["name"])
+    end
+
+    redirect "/owners/#{@owner.id}"
   end
 
-  get '/owners/:id/edit' do 
-    @owner = Owner.find(params[:id])
-    erb :'/owners/edit'
+  #READ
+
+  get '/owners' do
+    @owners = Owner.all
+    erb :'/owners/index' 
   end
 
   get '/owners/:id' do 
@@ -23,7 +29,24 @@ class OwnersController < ApplicationController
     erb :'/owners/show'
   end
 
+  #UPDATE
+
+  get '/owners/:id/edit' do 
+    @owner = Owner.find(params[:id])
+    erb :'/owners/edit'
+  end
+
   patch '/owners/:id' do 
-   
+    if !params[:owner].keys.include?("pet_ids")
+      params[:owner]["pet_ids"] = []
+    end
+
+    @owner = Owner.find(params[:id])
+    @owner.update(params["owner"])
+    if !params["pet"]["name"].empty?
+      @owner.pets << Pet.create(name: params["pet"]["name"])
+    end
+    redirect "owners/#{@owner.id}"
+    
   end
 end
